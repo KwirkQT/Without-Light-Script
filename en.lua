d49756d148d0ec66d745a85b32d4abe8df192c74
KwirkQT <34048868+KwirkQT@users.noreply.github.com>
2017-11-27 19:24:21 -0800

    Create en.lua

---
+++ en.lua
@@ -0,0 +1,11 @@
+Text_1_1 “Are you afraid of the dark?”
+
+Text_1_2 “Nobody seems to understand the darkness. They don’t understand the edges of the map...The unknown that both inspires and scares us. I understand the contours of this room. There is nothing surprising about it and that’s how I like it.”
+
+Text_1_3 “I am alone and this is best for me. It’s easier. Safer”
+
+#Select picture 1 (family): Text_1_4 “Blood is thicker than water, or so they say. I see my family more than almost anyone else. I wonder if they see me too? Sometimes I am not sure.”
+
+#Select picture 2 (friends): Text_1_5 “I miss the way we were. Sometimes I even miss the way I used to be.”
+
+#Select picture 3 (beach): Text_1_6 “I remember the day we went to the beach. That was a fun day. I was able to focus back then on enjoying myself. I felt the sand beneath my feet and watched it take shape around my toes. It was coarse, but inviting. There was no worry that day. No shadows or hidden fears. The light of the sun burned so brightly that day that I swear I was floating. I wish time could have stood still then. I wish I could live that day forever.” (Light goes up)

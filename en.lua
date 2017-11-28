d40e4d40ff7d0c9a6c087d072cf5496edec899d1
KwirkQT <34048868+KwirkQT@users.noreply.github.com>
2017-11-27 19:25:44 -0800

    Update en.lua

--- en.lua
+++ en.lua
@@ -4,8 +4,8 @@ Text_1_2 “Nobody seems to understand the darkness. They don’t understand the
 
 Text_1_3 “I am alone and this is best for me. It’s easier. Safer”
 
-#Select picture 1 (family): Text_1_4 “Blood is thicker than water, or so they say. I see my family more than almost anyone else. I wonder if they see me too? Sometimes I am not sure.”
+#Select picture 1 (family): Text_1_family “Blood is thicker than water, or so they say. I see my family more than almost anyone else. I wonder if they see me too? Sometimes I am not sure.”
 
-#Select picture 2 (friends): Text_1_5 “I miss the way we were. Sometimes I even miss the way I used to be.”
+#Select picture 2 (friends): Text_1_friends “I miss the way we were. Sometimes I even miss the way I used to be.”
 
-#Select picture 3 (beach): Text_1_6 “I remember the day we went to the beach. That was a fun day. I was able to focus back then on enjoying myself. I felt the sand beneath my feet and watched it take shape around my toes. It was coarse, but inviting. There was no worry that day. No shadows or hidden fears. The light of the sun burned so brightly that day that I swear I was floating. I wish time could have stood still then. I wish I could live that day forever.” (Light goes up)
+#Select picture 3 (beach): Text_1_beach “I remember the day we went to the beach. That was a fun day. I was able to focus back then on enjoying myself. I felt the sand beneath my feet and watched it take shape around my toes. It was coarse, but inviting. There was no worry that day. No shadows or hidden fears. The light of the sun burned so brightly that day that I swear I was floating. I wish time could have stood still then. I wish I could live that day forever.” (Light goes up)
